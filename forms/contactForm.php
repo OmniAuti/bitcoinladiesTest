bae50f5d35a5ea192ca05fcf5f4010d653ff33cd
Austin Olijar <Austin.Olijar@gmail.com>
2022-07-17 09:52:22 -0500

    Add files via upload

--- forms/contactForm.php
+++ forms/contactForm.php
@@ -9,7 +9,7 @@
     $headers = "New Contact Us Submission";
     $txt = "You have received a contact us submmission from ".$name.".\n
     Their email is ".$email." \n
-    They are writing to say: \n".$info.;
+    They are writing to say: \n"$info;
                             
     
     mail($to, "New Contact Us Submission", $txt);
